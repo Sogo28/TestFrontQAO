500bc7e50a3f8d52a771e8c463e79b1a4d1a565e
Daniel Andre Sogo DIEME <danielandresogodieme@esp.sn>
2024-09-11 09:08:24 +0000

    Configuration de firebase pour l'authentification

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnN3TXAZIm4d8VED1pNXm7RZ2rB1fOeIw",
+  authDomain: "art-institute-of-chicago.firebaseapp.com",
+  projectId: "art-institute-of-chicago",
+  storageBucket: "art-institute-of-chicago.appspot.com",
+  messagingSenderId: "945869149022",
+  appId: "1:945869149022:web:09ef29c357c7b23d8ef5cf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
